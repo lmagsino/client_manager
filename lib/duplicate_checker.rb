4f73c7449094253120d551135305495fa0ee26e8
Leo Magsino <leo.magsino819@gmail.com>
2024-09-22 11:34:28 +0800

    Apply index to duplicate checker

--- lib/duplicate_checker.rb
+++ lib/duplicate_checker.rb
@@ -1,10 +1,23 @@
 class DuplicateChecker
   def initialize(clients)
     @clients = clients
+    build_email_index
   end
 
+  # Build an index for quick lookups by email
+  def build_email_index
+    @email_index = Hash.new { |hash, key| hash[key] = [] }
+
+    @clients.each do |client|
+      email = client['email']
+      # Index clients by email
+      @email_index[email] << client if email
+    end
+  end
+
+  # Find duplicates based on indexed emails
   def find_duplicates
-    grouped_by_email = @clients.group_by { |client| client['email'] }.reject { |email, clients| email.nil? }
-    grouped_by_email.select { |email, clients| clients.size > 1 }
+    # Select emails with more than one associated client
+    @email_index.select { |_, clients| clients.size > 1 }
   end
 end
